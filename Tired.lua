8bd92bd1c5748471138e4e941d192e13acaae9ca
ItsDismane <weathercraft9@gmail.com>
2025-07-19 16:47:20 +0200

    Delete Tired.lua

--- Tired.lua
+++
@@ -1,8 +0,0 @@
-local player = game.Players.LocalPlayer
-local hrp = player.Character.HumanoidRootPart
-
-local flySpeed = 50
-
-game:GetService("RunService").RenderStepped:Connect(function()
-    hrp.Velocity = Vector3.new(0, flySpeed, 0) -- drží hráča vo vzduchu
-end)
